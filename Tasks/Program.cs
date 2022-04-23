a324317304500724736bd84286399566f34fe590
AlexanderLobach <375296255246@ya.ru>
2022-04-24 01:23:50 +0300

    Task_1 is solved

---
+++ Tasks/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Константин!");
+            int num = Task_1.StringToNumber("22");
+            Console.WriteLine("The number = {0}",num);
+        }
+    }
+}
