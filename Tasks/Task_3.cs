a715623aa5277766576731709dbf4650c78ef6af
AlexanderLobach <375296255246@ya.ru>
2022-04-27 12:16:32 +0300

    Task_3 is solved

--- Tasks/Task_3.cs
+++ Tasks/Task_3.cs
@@ -4,6 +4,7 @@ public class Converter
 {
     public static string dnaToRna(string str)
     {
+        Console.WriteLine(str.Length);
         char[] char_arr;
         char_arr = str.ToCharArray();
         int i = 0;
@@ -12,10 +13,11 @@ public class Converter
             if (item == 'U')
             {
                 char_arr[i] = 'T';
-                i++;
             }
+            i++;
         }
-        str = Convert.ToString(str);
+        str = new string(char_arr);
+        Console.WriteLine(str.Length);
         return str;
     }
 }
