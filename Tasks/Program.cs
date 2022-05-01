5a420f359d998a0ccf2d7126f216d6a48c762d78
AlexanderLobach <375296255246@ya.ru>
2022-05-01 10:50:12 +0300

    Task_8 is solved

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -19,6 +19,7 @@ namespace Tasks
             Task_5.MakeToUpper("branch");
             Task_6.Liters(1.0);
             Task_7.TotalPoints(new[]{"1:1", "2:2", "3:3", "4:4", "2:2", "3:3", "4:4", "3:3", "4:4", "4:4" });
+            Task_8.Solution("Kozel");
         }
     }
 }
--- Tasks/Task_8.cs
+++ Tasks/Task_8.cs
@@ -2,5 +2,17 @@ using System;
 
 public class Task_8
 {
-    
+    public static string Solution(string str)
+    {
+        char[] temp = new char[str.Length];
+        int x = str.Length-1;
+        foreach (var item in str)
+        {
+            temp[x] = item;
+            x--;
+        }
+        str = String.Concat(temp);
+        Console.WriteLine("Task_8 result: {0}", str);
+        return str;
+    }
 }
